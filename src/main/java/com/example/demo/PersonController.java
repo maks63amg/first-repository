f67bbc63137ca734323fbe360fa833f0fa3d903c
Maks Oliferuk <maksimoliferuk63@gmail.com>
2022-02-03 12:03:27 +0200

    change PersonController

--- src/main/java/com/example/demo/PersonController.java
+++ src/main/java/com/example/demo/PersonController.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
+    @GetMapping("/sayHello")
+    private String hello(){
+        return "hello, guys";
+    }
 }
