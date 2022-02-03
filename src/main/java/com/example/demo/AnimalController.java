44b9bac80e59c607a6a5817ef14566cc133a4629
Maks Oliferuk <maksimoliferuk63@gmail.com>
2022-02-03 12:45:58 +0200

    add AnimalController

---
+++ src/main/java/com/example/demo/AnimalController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/animal")
+public class AnimalController {
+    @GetMapping("/kind")
+    private String animalKind(){
+        return "dog";
+    }
+}
